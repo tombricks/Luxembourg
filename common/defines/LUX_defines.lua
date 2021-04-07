749f83749c3ae8d2fee0557fc0189f5c0330b745
tombricks <tombricks@outlook.com>
2021-04-07 05:17:24 +0100

    Gamer

--- common/defines/LUX_defines.lua
+++ common/defines/LUX_defines.lua
@@ -2,3 +2,7 @@ NDefines.NGame.START_DATE = "1900.1.1.12"
 NDefines.NGame.END_DATE = "2100.1.1.12"
 NDefines.NCountry.EVENT_PROCESS_OFFSET = 1
 NDefines_Graphics.NFrontend.NEW_GAME_BUTTON_DISABLE_DELAY_ON_INVALID_MAP_DATA = 0.5
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 20} -- After this amount of VP the map icon becomes bigger dot.
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {1250, 1250, 120050}  -- At what camera distance the VP name text disappears.
+NDefines_Graphics.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {5000, 5000, 5000} -- At what distance VPs are hidden
+NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 10000 -- At what distance VPs are hidden
